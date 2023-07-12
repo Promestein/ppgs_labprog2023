3894328a482cdc25f0639b4b1d1ddb3d9c116e71
Marcus <marcus.oliveira@nca.ufma.br>
2023-07-12 10:42:45 -0300

    [Dev] Marcus Oliveira - Terminando Keycloak.

---
+++ backend/sppg/src/main/java/br/ufma/sppg/controller/TecnicaController.java
@@ -0,0 +1,35 @@
+// package br.ufma.sppg.controller;
+
+// import java.util.List;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import br.ufma.sppg.model.Producao;
+// import br.ufma.sppg.service.ProducaoService;
+// import br.ufma.sppg.service.exceptions.ServicoRuntimeException;
+
+// @RestController
+// @CrossOrigin(origins = "*", allowedHeaders = "*")
+// @RequestMapping("/api/producao")
+// public class ProducaoController {
+//     @Autowired
+//     ProducaoService producaoServ;
+
+//     @GetMapping("/obterProducao")
+//     public ResponseEntity obterPrograma() {
+//         try {
+//             List<Producao> producoes = producaoServ.retornarTodasAsProducoes();
+//             return new ResponseEntity(producoes, HttpStatus.OK);
+//         } catch (ServicoRuntimeException e) {
+//             return ResponseEntity.badRequest().body(e.getMessage());
+//         }
+//     }
+
+
+// }
